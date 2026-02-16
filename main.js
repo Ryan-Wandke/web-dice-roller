fa03f8c6600e6a1acb382de9d9768411bca2a753
Ryan-Wandke <ryanwandke@gmail.com>
2026-02-15 23:41:40 -0600

    added comments, camelcase, and better indentation along with a few minor code tweaks

--- main.js
+++ main.js
@@ -3,53 +3,50 @@ let attack = 1
 let defense  = 1
 
 function start(){
-        makeattacker()
-        makedefender()
-        displayDice()
+    makeAttacker()
+    makeDefender()
+    displayDice()
+    //displayDice() also gets called whenever the roll button is pressed.  
 
 }
 
 start()
 
 
-function makeattacker(){
+function makeAttacker(){
     document.getElementById("selectorA").innerHTML = `<p>Attacker</p>
-                <select name="Attacker" id="Attacker" onchange="setAttackDice(this.value)">
-                <option value="1">1</1>
-                <option value="2">2</2>
-                <option value="3">3</3>
-                </select>`
+        <select name="Attacker" id="Attacker" onchange="setAttackDice(this.value)">
+        <option value="1">1</1>
+        <option value="2">2</2>
+        <option value="3">3</3>
+        </select>`
 }
 
-function makedefender(){
+function makeDefender(){
     document.getElementById("selectorD").innerHTML = `<p>Defender</p>
-                <select name="Defender" id="Defender" onchange="setDefenseDice(this.value)">
-                <option value="1">1</1>
-                <option value="2">2</2>
-                </select>`
+        <select name="Defender" id="Defender" onchange="setDefenseDice(this.value)">
+        <option value="1">1</1>
+        <option value="2">2</2>
+        </select>`
 }
 
 function setDefenseDice(number){
-    document.getElementById("selectorD")
     defense = number
 }
 function setAttackDice(number){
-    document.getElementById("selectorA")
    attack = number
 }
 
 function displayDice(){
     document.getElementById("dice").innerHTML = `
-    <p> Attackers Dice </p>
-            ${Stringbuild(attack)}
-            <p> Defenders Dice </p>
-            ${Stringbuild(defense)}`
+    <p> Attackers Dice </p>${stringBuild(attack)}<p> Defenders Dice </p>${stringBuild(defense)}`
 }
-function Stringbuild(number){
+function stringBuild(number){
     x = number
     y = ""
     while(x > 0){
         y+= `<img src= "dice${Math.ceil(Math.random() * 6)}.jpg">`
+        //this builds a number of strings to display dice equal to the number set by the number input
         x--
     }
     return y
